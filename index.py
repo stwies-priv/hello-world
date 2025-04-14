7ba319f1c23fdf9ce10cd557c2c7728cf61dc475
Stephan Wiesneth <stephan.wiesneth@prodato.de>
2025-04-14 14:53:04 +0200

    Added image to Hello World

--- index.py
+++ index.py
@@ -4,3 +4,6 @@ print('This is the first file in my new Git Repo \n')
 print('\n Diese Zeile ist ein Update des Files. \n')
 
 print('\n Und noch eine Zeile. \n')
+
+print('\n Drittes Update. Hier wuerde theoretisch ein Bild hinzugefügt werden, soll aber in meiner Version einfach gehalten werden. \n')
+
